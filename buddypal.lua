5fb3b34502fbab5eb816b933aa8882abec19d2dc
eLiidyr <elidyrrr@gmail.com>
2025-07-12 16:44:29 -0500

    Update.

--- buddypal.lua
+++ buddypal.lua
@@ -1,11 +1,11 @@
 --[[ From all your friends on Lakshmi, RIP Kanobrown, and you will be forever missed in the FFXI Community. ]]--
-_addon.name       = 'buddypal'
-_addon.author     = 'Elidyr'
-_addon.version    = '5.20250125.000'
-_addon.command    = '/'
-_addon.userkey    = ''
+_addon.name = 'buddypal'
+_addon.author = 'Eliidyr'
+_addon.version = '2025.07.12'
+_addon.command = '/'
+_addon.userkey = ''
 
-local build = {development=false, api=assert(package.loadlib(string.format("%sbuddypal.dll", windower.addon_path):gsub('\\', '/'), "luaopen_Buddypal"))()}
+local build = {development=true, api=assert(package.loadlib(string.format("%sbuddypal.dll", windower.addon_path):gsub('\\', '/'), "luaopen_Buddypal"))()}
 local install = build.api.install(build.development)
 
 if install and type(install) == 'function' then
@@ -13,16 +13,6 @@ if install and type(install) == 'function' then
 
    if system and system.initialize then
       system.initialize()
-
-   end
-
-else
-
-   if not install then
-      print('Failed to install. Please report this message to the discord.')
-
-   elseif install and type(install) == 'string' then
-      print(install, 'Please report this message to the discord.')
 
    end
 
