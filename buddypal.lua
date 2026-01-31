ed5f3a603e09e9cccb25459d1c8116fca82e6df6
eLiidyr <elidyrrr@gmail.com>
2026-01-31 16:30:18 -0600

    Alpha release.

--- buddypal.lua
+++ buddypal.lua
@@ -1,19 +1,7 @@
---[[ From all your friends on Lakshmi, RIP Kanobrown, and you will be forever missed in the FFXI Community. ]]--
-_addon.name = 'buddypal'
-_addon.author = 'Eliidyr'
-_addon.version = '2025.07.12'
-_addon.command = '/'
-_addon.userkey = ''
-
-local build = {development=true, api=assert(package.loadlib(string.format("%sbuddypal.dll", windower.addon_path):gsub('\\', '/'), "luaopen_Buddypal"))()}
-local install = build.api.install(build.development)
-
-if install and type(install) == 'function' then
-   local system = install(build)
-
-   if system and system.initialize then
-      system.initialize()
-
-   end
-
-end
+_addon.name    = 'buddypal'
+_addon.author  = 'Eliidyr'
+_addon.command = 'bp'
+local buddypal = assert(package.loadlib(string.format("%splugin.dll", windower.addon_path):gsub('\\', '/'), "luaopen_Buddypal"))()
+local init     = buddypal.install(windower.addon_path)
+init(buddypal)
+buddypal.connect(windower.addon_path)
